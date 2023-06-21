4349013be6f115cbd32bafb61820b7b6d9cc602a
owlot <95369289+owlot@users.noreply.github.com>
2023-06-21 18:38:27 +0200

    refactor: use lib-common and new svc-storage-grpc-client
    [skip ci]

--- server/build.rs
+++ server/build.rs
@@ -3,7 +3,7 @@
 ///generates .rs files in src directory
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let proto_dir = "../proto";
-    let proto_file = &format!("{}/svc-assets-grpc.proto", proto_dir);
+    let proto_file = &format!("{}/grpc.proto", proto_dir);
 
     let server_config = tonic_build::configure()
         .type_attribute("ReadyRequest", "#[derive(Eq, Copy)]")
@@ -11,6 +11,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client_config = server_config.clone();
 
     client_config
+        .client_mod_attribute("grpc", "#[cfg(not(tarpaulin_include))]")
         .build_server(false)
         .out_dir("../client-grpc/src/")
         .compile(&[proto_file], &[proto_dir])?;
--- server/tests/integration_test.rs
+++ server/tests/integration_test.rs
@@ -1,9 +1,43 @@
-//! Example for writing an integration test.
-//! More information: https://doc.rust-lang.org/book/testing-rust.html#integration-tests
+//! Integration Tests
 
-// use tmp_lib;
+fn get_log_string(function: &str, name: &str) -> String {
+    #[cfg(feature = "stub_server")]
+    return format!("({} MOCK) {} server.", function, name);
 
-// #[test]
-// fn it_add_one() {
-//     assert_eq!(2, tmp_lib::add_one(1));
-// }
+    #[cfg(not(feature = "stub_server"))]
+    return format!("({}) {} server.", function, name);
+}
+
+#[tokio::test]
+async fn test_server_requests_and_logs() {
+    use logtest::Logger;
+    use svc_assets::grpc::server::*;
+
+    let name = "assets";
+
+    // Start the logger.
+    let mut logger = Logger::start();
+
+    //test_is_ready_request_logs
+    {
+        let imp = ServerImpl::default();
+        let result = imp.is_ready(tonic::Request::new(ReadyRequest {})).await;
+        assert!(result.is_ok());
+        let result: ReadyResponse = result.unwrap().into_inner();
+        assert_eq!(result.ready, true);
+
+        // Search for the expected log message
+        let expected = get_log_string("is_ready", name);
+        println!("expected message: {}", expected);
+        assert!(logger.any(|log| {
+            if log.target().contains("app::") {
+                println!("{}", log.target());
+                let message = log.args();
+                println!("{:?}", message);
+                log.args() == expected
+            } else {
+                false
+            }
+        }));
+    }
+}
