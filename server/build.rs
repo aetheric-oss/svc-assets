660bece8217d0ef1e6813c28c8c1496e123a197d
AM Smith <smitham42@gmail.com>
2023-09-26 11:46:32 -0700

    fix: remove (build) from build.rs print statement

--- server/build.rs
+++ server/build.rs
@@ -21,7 +21,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_client(false)
         .compile(&[proto_file], &[proto_dir])?;
 
-    println!("(build) cargo:rerun-if-changed={}", proto_file);
+    println!("cargo:rerun-if-changed={}", proto_file);
 
     Ok(())
 }
