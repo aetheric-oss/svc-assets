9de9f79b0f0184263fe97e4fc438279ac5ae0c6e
owlot <95369289+owlot@users.noreply.github.com>
2023-10-30 21:09:58 +0100

    feat: add prelude module

--- client-grpc/tests/integration_test.rs
+++ client-grpc/tests/integration_test.rs
@@ -18,25 +18,20 @@ fn get_log_string(function: &str, name: &str) -> String {
 async fn test_client_requests_and_logs() {
     use logtest::Logger;
 
-    use svc_assets_client_grpc::service::Client as ServiceClient;
-    use svc_assets_client_grpc::*;
-    use tonic::transport::Channel;
-
-    std::env::set_var("RUST_LOG", "debug");
+    use svc_assets_client_grpc::prelude::*;
 
     let name = "assets";
     let (server_host, server_port) =
         lib_common::grpc::get_endpoint_from_env("GRPC_HOST", "GRPC_PORT");
 
-    let client: GrpcClient<RpcServiceClient<Channel>> =
-        GrpcClient::new_client(&server_host, server_port, name);
+    let client = AssetsClient::new_client(&server_host, server_port, name);
 
     // Start the logger.
     let mut logger = Logger::start();
 
     //test_is_ready_request_logs
     {
-        let result = client.is_ready(ReadyRequest {}).await;
+        let result = client.is_ready(assets::ReadyRequest {}).await;
         println!("{:?}", result);
         assert!(result.is_ok());
 
